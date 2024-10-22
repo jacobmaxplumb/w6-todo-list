7205ad187bd1eb4318ab646871af123f9a8bda20
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-10-22 18:58:56 -0400

    almost finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodoItem = () => {
+    setTodos([...todos, { id: Date.now(), text: text, completed: false }]);
+    setText("");
+  };
+
+  const removeTodoItem = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const markTodoAsCompleted = (id) => {
+    const todoItems = todos.map((todo) => {
+      if (todo.id === id) return { ...todo, completed: true };
+      else return { ...todo };
+    });
+    setTodos(todoItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodoItem}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            onClick={() => {
+              removeTodoItem(todo.id);
+            }}
+            key={todo.id}
+          >
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
